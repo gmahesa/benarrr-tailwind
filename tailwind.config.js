1f26c6d617304eec3d8ebbf373996935b0127973
gmahesa <galangmahesa99@gmail.com>
2023-10-23 13:31:56 +0800

    fix style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}", ".html"],
+  content: ["./src/**/*.{html,js}", "*.html"],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+        custom: "0px 0px 50px -2px rgba(5, 7, 41, 0.11)",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      boxShadow: ["hover"],
+      keyframes: {
+        slide: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+      },
+    },
   },
   plugins: [],
 };
